9d9ee1639f065cdb621e8aecbf7032d8b64dbe7f
blue <dylancuprewich@gmail.com>
2018-09-26 16:57:21 -0400

    main.py completed. Maybe document more?

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import numpy
+import numpy as np # For array functions and such
+from PIL import Image # For creation of images
+import imageio as imio # For appending images together to form an animated gif
+import time # For timing how long entire program takes to execute
 
 def main():
-    matrix = numpy.zeros( ( 400 , 400 ) )
+    # variables for frame creation and writing, as well as gif creation and writing
+    w, h = 1024,1024
+    all_images = []
+    total_num_frames = 30
+    frame_extension = '.png'
+    gif_location = 'rando.gif'
+    
+    # creation of frames and appending frames to the gif
+    with imio.get_writer( gif_location , mode='I') as writer:
+        for i in range( total_num_frames):
+            file_path = str(i)+frame_extension
+            Image.fromarray(
+                (np.random.rand( w, h, 3)*255).astype( 
+                    np.int8 ) ,'RGB').save(
+                        file_path, 'PNG')
+            writer.append_data(imio.imread(file_path ))
 
-    print(matrix)
-
+start_time = time.time()
 main()
+print("Time to complete: {}".format(time.time()-start_time))
